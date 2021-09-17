761896e3305f3c1b952c826c176b1093631a8e89
xshi0603 <xshi603@gmail.com>
2021-09-17 01:26:12 -0400

    assignment4

---
+++ assignment4/my_turtles.py
@@ -0,0 +1,45 @@
+
+#Name:  Xing Tao Shi
+#Email:  XingTao.Shi50@myhunter.cuny.edu
+#Date: August 27, 2021
+#This program 
+
+import turtle
+
+tess = turtle.Turtle()           # create tess and set some attributes
+
+tess.shape("arrow")
+tess.color("purple")
+tess.pensize(3)
+
+tess.forward(30)
+
+for i in range(3):
+  tess.forward(50)
+  tess.right(120)
+
+tess.backward(30)
+tess.right(90)
+tess.forward(30)
+
+for i in range(3):
+  tess.forward(50)
+  tess.right(120)
+
+tess.backward(30)
+tess.right(90)
+tess.forward(30)
+
+for i in range(3):
+  tess.forward(50)
+  tess.right(120)
+
+tess.backward(30)
+tess.right(90)
+tess.forward(30)
+
+for i in range(3):
+  tess.forward(50)
+  tess.right(120)
+
+tess.backward(30)
