c9a558ef3bfdfa152944879215dd7ec99e24af21
xshi0603 <xshi603@gmail.com>
2021-09-30 13:06:57 -0400

    assignment 7

---
+++ assignment7/increment.py
@@ -0,0 +1,7 @@
+#Name:  Xing Tao Shi
+#Email:  XingTao.Shi50@myhunter.cuny.edu
+#Date: August 27, 2021
+#This program 
+
+for i in range(2, 22, 2):
+  print("current value: " + str(i) + " | value+10: " + str(i + 10))
---
+++ assignment7/shift.py
@@ -0,0 +1,9 @@
+#Name:  Xing Tao Shi
+#Email:  XingTao.Shi50@myhunter.cuny.edu
+#Date: August 27, 2021
+#This program 
+
+mess1 = input('Enter a message: ')
+
+for i in (mess1):
+  print("" + i + " shifted by 5 characters is: " + chr(ord(i) + 5))
